b9110140e6507f136ca66890514d4067c42ba468
Kacper Gumulak <168137361+Zorquan04@users.noreply.github.com>
2024-12-12 00:53:15 +0100

    Stworzenie wstępnej struktury (Dziekan, Komisja, Student)

---
+++ Egzamin/StudentApp/main_student.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdlib>
+#include <ctime>
+#include "utils.h"
+#include "structures.h"
+#include "constants.h"
+
+int main()
+{
+	srand(time(nullptr)); // inicjalizacja generatora losowego
+	int student_id = rand() % 1000 + 1;
+
+	cout << "Student o ID = " << student_id << " gotowy na egzamin..." << endl;
+
+	// obsługa komunikacji z dziekanem i komisją
+
+	return 0;
+}
---
+++ Egzamin/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cerrno>
+using namespace std;
+
+void handle_error(const string&);
+
+#endif
+
+//asdasdad
