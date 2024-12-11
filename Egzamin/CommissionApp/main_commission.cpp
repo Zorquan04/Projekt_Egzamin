140801ec12c5bf269aee4215ceacd2dd4d461c00
Kacper Gumulak <168137361+Zorquan04@users.noreply.github.com>
2024-12-12 00:45:44 +0100

    Stworzenie wstępnej struktury (Dziekan, Komisja, Student)

---
+++ Egzamin/CommissionApp/main_commission.cpp
@@ -0,0 +1,12 @@
+﻿#include "utils.h"
+#include "structures.h"
+#include "constants.h"
+
+int main()
+{
+	cout << "Komisja uruchomiona i gotowa do pracy..." << endl;
+
+	//obsługa przyjmowania studentów
+
+	return 0;
+}
---
+++ Egzamin/DeanApp/main_dean.cpp
@@ -0,0 +1,14 @@
+﻿#include "utils.h"
+#include "structures.h"
+#include "constants.h"
+
+int main()
+{
+	cout << "Dziekan uruchamia proces egzaminu..." << endl;
+
+	// inicjalizacja pamięci wspóldzielonej i kolejek komunikatów
+	// odbieranie wyników od komisji
+	// publikowanie wyników
+
+	return 0;
+}
