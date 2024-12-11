0b4b6943846c40aa5fb741c6793299c06f269671
Kacper Gumulak <168137361+Zorquan04@users.noreply.github.com>
2024-12-12 00:52:14 +0100

    Delete Egzamin directory

--- Egzamin/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-
-void handle_error(const string& message) // obs³uga b³êdów
-{
-	char error_buf[256]; // bufor na komunikat b³edu
-	strerror_s(error_buf, sizeof(error_buf), errno); // zapis komunikatu do bufora
-
-	cerr << message << ": " << error_buf << endl;
-}
